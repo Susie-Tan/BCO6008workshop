0b66aac41e1bac8849b8adad0f163c7b403cd1a4
Susie Tan <gracesust@gmail.com>
2021-08-04 18:51:18 +1000

    create Github

---
+++ workshop1.R
@@ -0,0 +1,12 @@
+install.packages("tidyverse") #installing packages (do ONCE!)
+install.packages("tidymodels")
+install.packages("skimr")
+install.packages("janitor")
+
+library(tidyverse)
+library(tidymodels)
+library(skimr)
+library(janitor)
+
+olympics<-read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
+olympics%>%head()
